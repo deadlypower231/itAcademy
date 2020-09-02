071e9f32b292c094983e5fb538048697c0bdee8e
Artofpaganini <Artofpaganini@gmail.com>
2020-09-02 21:15:25 +0300

    sdfsdf

---
+++ src/Animals/Dog.java
@@ -0,0 +1,14 @@
+package Animals;
+
+public class Dog {
+
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Animals/Main.java
@@ -0,0 +1,13 @@
+package Animals;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        Dog dog = new Dog();
+        dog.setName("Barsik");
+
+    }
+
+}
