20ed0c497f2b547affe0acd58804edd44bee1121
deadlypower231 <deadlypower1992@gmail.com>
2020-10-13 23:19:58 +0300

    Update SerializationUtils.java

--- src/SerializationUtils/SerializationUtils.java
+++ src/SerializationUtils/SerializationUtils.java
@@ -8,24 +8,19 @@ import java.io.ObjectOutputStream;
 public class SerializationUtils {
 
     public static void serialization(Object object1, String name) {
-
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("src\\FightingAnimals\\save\\" + name + ".info"))) {
             objectOutputStream.writeObject(object1);
-            System.out.println("serialization");
         } catch (Exception e) {
             System.out.println("Serialization was failed!");
         }
-
     }
 
     public static Object deserialization(String name) {
         try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("src\\FightingAnimals\\save\\" + name + ".info"))) {
             return objectInputStream.readObject();
-
         } catch (Exception e) {
             System.out.println("Deserialization was failed!");
             return null;
         }
-
     }
 }
